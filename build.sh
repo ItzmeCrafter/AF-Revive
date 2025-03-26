0cf73c50b4cc12a40a6fae68298c81415bdddf26
Aziangelo <aziangelo@gmail.com>
2025-03-27 01:20:06 +0800

    file

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 LD_LIBRARY_PATH=./tool/lib python3 tool $@
 
 # Define source and destination
-SRC="build/android/"
-DEST="/storage/emulated/0/games/com.mojang/resource_packs/AziFy-Revi/renderer/materials/"
+#SRC="build/android/"
+#DEST="/storage/emulated/0/games/com.mojang/resource_packs/AziFy-Revi/renderer/materials/"
 
 # Copy files source to destination (overwrite existing files)
-cp -r "$SRC"* "$DEST"
+#cp -r "$SRC"* "$DEST"
 
-echo "Files copied successfully!"
+#echo "Files copied successfully!"
