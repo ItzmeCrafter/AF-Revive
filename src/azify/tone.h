1c7ba3257f4fcaa581ae4523752a4ab083b8d70e
Aziangelo <aziangelo@gmail.com>
2025-03-26 21:49:44 +0800

    win fix

--- src/azify/tone.h
+++ src/azify/tone.h
@@ -21,20 +21,4 @@ vec3 sat(vec3 c, float a) {
     vec3 i = vec3(x,x,x);
     return min(mix(i, c, a), vec3(1.,1.,1.));}
 
-vec3 ACESFilmToneMapping(vec3 c) {
-    const mat3 ACESInMat = mat3(
-        0.59719, 0.07600, 0.02840,
-        0.35458, 0.90834, 0.13383,
-        0.04823, 0.01566, 0.83777
-    );
-    const mat3 ACESOutMat = mat3(
-        1.60475, -0.10208, -0.00327,
-        -0.53108,  1.10813, -0.07276,
-        -0.07367, -0.00605,  1.07602
-    );
-    c = ACESInMat * c;
-    c = (c * (c + 0.0245786)) / (c * (0.983729 * c + 0.4329510) + 0.238081);
-    c = ACESOutMat * c;
-    return clamp(c, 0.0, 1.0);}
-
 #endif
